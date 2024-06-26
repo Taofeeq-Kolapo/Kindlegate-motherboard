34e7d53e827dc9078db633b4d541b31c9d267582
Taofeeq001 <taofeeqkolapo@gmail.com>
2024-06-25 17:04:59 -0700

    updated blog page

---
+++ src/app/blogs/page.js
@@ -0,0 +1,14 @@
+import BlogBody from '@/components/Blogs/BlogBody'
+import BlogHero from '@/components/Blogs/BlogHero'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <BlogHero/>
+        <BlogBody/>
+    </div>
+  )
+}
+
+export default page
