fc1441027b9c4488e93ed24e84219b7f0c762e8a
Taofeeq001 <taofeeqkolapo@gmail.com>
2024-07-15 10:46:38 -0700

    updating...

--- src/components/Forms/Attestation.jsx
+++ src/components/Forms/Attestation.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 const Attestation = () => {
+    const [applicant, setApplicant] = useState("")
     return (
         <div className='px-[10%] py-6 flex flex-col gap-3'>
             <p className='text-[30px] font-bold text-black'>KINDLEGATE  FONDATION APPLICANT ATTESTATION</p>
@@ -19,7 +21,57 @@ const Attestation = () => {
                     <p>I understand that Membership category awarded will be revoked for failure to renew Membership and that I cannot hold myself out as a member if I am not an active member in good standing.</p>
                     <p>Having read through this, I hereby accept the obligation to observe the regulations of the Organization as set out in it’s Memorandum and Article of Association, this Membership form and any determination of the Governing Council.</p>
                 </div>
-
+                <div>
+                    <div className='flex flex-col gap-2'>
+                        <span className='flex flex-row items-center gap-1'>
+                            <p>Applicant Signature:</p>
+                            <input required type="text"
+                                value={applicant}
+                                onChange={(e) => setApplicant(e.target.value)} className='w-[80%] outline-none border-b-2 border-black bg-transparent' name="" id="" />
+                        </span>
+                        <div className='w-full grid grid-cols-3 gap-2'>
+                            <span className='flex flex-row items-center gap-1'>
+                                <p>Date:</p>
+                                <input required type="text" className='w-[80%] outline-none border-b-2 border-black bg-transparent' name="" id="" />
+                            </span>
+                            <span className='flex flex-row items-center gap-1'>
+                                <p>Day of</p>
+                                <input required type="text" className='w-[60%] outline-none border-b-2 border-black bg-transparent' name="" id="" />
+                            </span>
+                            <span className='flex flex-row items-center gap-1'>
+                                <p>202</p>
+                                <input required type="text" className='w-[84%] outline-none border-b-2 border-black bg-transparent' name="" id="" />
+                            </span>
+                        </div>
+                        <span className='flex flex-row items-center gap-1'>
+                            <p>Witness Signature:</p>
+                            <input required type="text" className='w-[82%] outline-none border-b-2 border-black bg-transparent' name="" id="" />
+                        </span>
+                        <div className='w-full grid grid-cols-3 gap-2'>
+                            <span className='flex flex-row items-center gap-1'>
+                                <p>Date:</p>
+                                <input required type="text" className='w-[80%] outline-none border-b-2 border-black bg-transparent' name="" id="" />
+                            </span>
+                            <span className='flex flex-row items-center gap-1'>
+                                <p>Day of</p>
+                                <input required type="text" className='w-[60%] outline-none border-b-2 border-black bg-transparent' name="" id="" />
+                            </span>
+                            <span className='flex flex-row items-center gap-1'>
+                                <p>202</p>
+                                <input required type="text" className='w-[84%] outline-none border-b-2 border-black bg-transparent' name="" id="" />
+                            </span>
+                        </div>
+                        <span className='flex flex-row items-center gap-1'>
+                            <input type="checkbox" name="" id="" />
+                            <p>I hereby <span className='font-bold'>Attest</span> to the above information </p>
+                        </span>
+                    </div>
+                </div>
+                <div className='w-full flex justify-center'>
+                    <button
+                        // onClick={HandleSubmitform}
+                        className='bg-[#0A2640] rounded-[56px] px-10 py-3 text-white font-bold'>Proceed to Attest</button>
+                </div>
             </form>
 
         </div>
