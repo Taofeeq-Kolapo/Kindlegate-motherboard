f04d17f17d894f611ba34b353b9f42df373d3dcb
Taofeeq001 <taofeeqkolapo@gmail.com>
2024-09-21 04:24:04 -0700

    updating changes

--- src/components/Contact/ContactHero.jsx
+++ src/components/Contact/ContactHero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ContactHero = () => {
     return (
         <div className="bg-[url(/img/bg-3.svg)] bg-no-repeat bg-cover bg-center h-screen justify-center flex items-center w-full ">
-            <div className="lg:pt-[5%] lg:px-[7%] flex w-full ">
+            <div className="lg:pt-[5%] lg:px-[7%] flex w-full justify-end ">
                 <div className="lg:w-[50%] flex flex-col w-full items-center gap-4">
                     <p className="font-bold md:text-[40px] text-[30px] text-white leading-none">REACH US</p>
                     <div className="flex flex-col items-center lg:w-[80%]">
